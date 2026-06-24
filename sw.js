fab8c108cf6b0ab13c54f262a7d9a276e47adf45
Genge Admin <admin@genge.local>
2026-06-24 13:19:06 -0700

    UI: Modernize feedback button and hide during checkout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'genge-cache-v12'; // bump version kila unapobadilisha files
+const CACHE_NAME = 'genge-cache-v13'; // bump version kila unapobadilisha files
 
 const urlsToCache = [
   '/',
